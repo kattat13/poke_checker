2f536978df0a602d9d46d4cb23f93d503028ea04
niekoniecznie <cyberbzdety@gmail.com>
2022-08-30 10:25:59 +0200

    static files fix

--- poke_checker/settings.py
+++ poke_checker/settings.py
@@ -116,18 +116,14 @@ USE_I18N = True
 
 USE_TZ = True
 
-#Static files
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.1/howto/static-files/
 STATIC_ROOT= os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'app/static')
+    os.path.join(BASE_DIR, 'poke_checker/static')
 ]
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.1/howto/static-files/
-
-STATIC_URL = "app/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
